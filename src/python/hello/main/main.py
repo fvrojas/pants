036287334f00a712347d5afa45bc38e65b5ad55d
Fabio Rojas <frojas@twitter.com>
2017-02-25 09:05:07 -0800

    Initial Commit with working example

---
+++ src/python/hello/main/main.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import sys
+
+from hello.greet.greet import greet
+
+
+if __name__ == '__main__':
+  greetees = sys.argv[1:] or ['world']
+  for greetee in greetees:
+    print(greet(greetee))
